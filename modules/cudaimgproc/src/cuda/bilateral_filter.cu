43fa993d4cdf686f458ca6fc2ef43a9e283e2fb7
Neelakandan <mythreyi@multicorewareinc.com>
2018-12-07 11:33:51 +0530

    Disable bilateral Kernel

--- modules/cudaimgproc/src/cuda/bilateral_filter.cu
+++ modules/cudaimgproc/src/cuda/bilateral_filter.cu
@@ -136,8 +136,10 @@ namespace cv { namespace cuda { namespace device
             float sigma_spatial2_inv_half = -0.5f/(sigma_spatial * sigma_spatial);
             float sigma_color2_inv_half = -0.5f/(sigma_color * sigma_color);
 
+#ifdef HIP_TODO
             cudaSafeCall( hipFuncSetCacheConfig (bilateral_kernel<T, B<T> >, hipFuncCachePreferL1) );
-            hipLaunchKernelGGL((bilateral_kernel), dim3(grid), dim3(block), 0, stream, (PtrStepSz<T>)src, (PtrStepSz<T>)dst, b, kernel_size, sigma_spatial2_inv_half, sigma_color2_inv_half);
+#endif
+            hipLaunchKernelGGL((bilateral_kernel<T, B>), dim3(grid), dim3(block), 0, stream, src, (PtrStepSz<unsigned char>)dst, b, kernel_size, sigma_spatial2_inv_half, sigma_color2_inv_half);
             cudaSafeCall ( hipGetLastError () );
 
             if (stream == 0)
